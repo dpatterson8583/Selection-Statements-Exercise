22fd50ec2b277a533c2a3febaf4d438bdcab023c
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-17 11:16:57 -0500

    Initial commit for Selection stmts 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.Write("Guess my favorite number. > ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput == 0)
+            {
+                Console.WriteLine("Game Over");
+            }
+            else if(userInput > 1000)
+            {
+                Console.WriteLine("Out of bounds");
+            }
+            else if(userInput<favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else if(userInput == favNumber)
+            {
+                Console.WriteLine("You got it Right!");
+            }
+            else 
+            { Console.WriteLine($"Nevermind...It was {favNumber}"); }
         }
     }
 }
